c36c34832618df4afc92b75b7cfe18618d2522f8
LuisLopezPacheco <71840410+LuisLopezPacheco@users.noreply.github.com>
2021-08-23 18:35:42 -0500

    Update lexico.cpp
    Corrección de errores, ahora las cantidades "11." son identificadas como enteros

--- MIni_Generador_Lexico/Mini_Generador_Lexico/lexico.cpp
+++ MIni_Generador_Lexico/Mini_Generador_Lexico/lexico.cpp
@@ -39,84 +39,114 @@ void Lexico::analyze()
 	state = 0;
 	index = 0;
 	next = true;
+	final = false;
 	result = "";
-	
-	while (index < size) {
-		if (next) {
-			c = nextSymbol();
-		}
 
-		if (isNum(c) and state == 0) {
-			state = 1;
-			result += c;
-			next = true;
-		}
-		else if (isNum(c) and state == 1) {
-			result += c;
-		}
-		else if ((isLyrics(c) or c ==164) and state == 0) {
-			state = 2; 
-			result += c;
-			next = true;
-		}
-		else if ((isLyrics(c) or c == 164) and state == 2) {
-			state = 2;
-			result += c;
-			next = true;
-		}
-		else if (isSpace(c)) {
+	while (final == false) {
+		
+			if (next) {
+				c = nextSymbol();
+			}
+
+			if (final == true) {
+				cout << "Final de la cadena" << endl;
+				break;
+			}
+
+			if (isNum(c) and state == 0) {
+				state = 1;
+				result += c;
+				next = true;
+			}
+			else if (isNum(c) and state == 1) {
+				result += c;
+			}
+			else if ((isLyrics(c) or c == 164) and state == 0) {
+				state = 2;
+				result += c;
+				next = true;
+			}
+			else if ((isLyrics(c) or c == 164) and state == 2) {
+				state = 2;
+				result += c;
+				next = true;
+			}
+			else if (isSpace(c)) {
+
+			}
+			else if (isPoint(c) and state == 1) {
+				state = 3;
+				result += c;
+			}
+			else if (isNum(c) and state == 3) {
+				state = 4;
+				result += c;
+			}
+			else if (isNum(c) and state == 4) {
+				result += c;
+			}
+			else if (state == -1) {
+				next = true;
+				result += c;
+			}
+			else if (isNum(c) or isLyrics(c) and state != 3) {
+				next = false;
+				behind();
+			}
+			else {
+				cout << "Se  obtuvo: " << result << " ";
+				if (state == 3) {
+					state = 1;
+				}
+				cout << chainResult() << endl;
+				result = "";
+				cout << c << ": Invalido, interrumpio la secuecia" << endl;
+				result += c;
+				state = -1;
+			}
+
+			if (next) {
+				if (isSpace(c)) {
+					cout << c << ": Espacio" << endl;
+				}
+				else {
+					cout << c << ": ";
+					Type();
+				}
+			}
+
 			
-		}
-		else if (isPoint(c) and state == 1){
-			state = 3;
-			result += c;
-		}
-		else if (isNum(c) and state == 3) {
-			state = 4;
-			result += c;
-		}
-		else if (isNum(c) and state == 4) {
-			result += c;
-		}
-		else if (state == -1) {
-			next = true;
-			result += c;
-		}
-		else if (isNum(c) or isLyrics(c) and state != 3) {
-			next = false;
-			behind();
-		}
-		else {
-			cout << "Se  obtuvo: " << result << " ";
-			cout << chainResult() << endl;
-			cout << c << ": Invalido, interrumpio la secuecia" << endl;
-			result += c;
-			state =-1;
-		}
 
-		if (next) {
-			cout << c << ": " ;
-			Type();
-		}
-		
-		
 	}
 
 	cout << "Se  obtuvo: " << result << " ";
+	if (state == 3) {
+		cout << "Entero" << endl;
+		state = 1;
+	}
 	cout << chainResult() << endl;
+	
 
 }
 
 
 char Lexico::nextSymbol()
 {
-	return myChain[index++];
+	if (index == size) {
+		next = false;
+		final = true;
+	}
+	else {
+		return myChain[index++];
+	}
+	
 }
 
 bool Lexico::isSpace(const char& c)
 {
 	return c== ' ' || c == '\t';
 }
+
 
 bool Lexico::isNum(const char& c)
 {
@@ -125,7 +155,7 @@ bool Lexico::isNum(const char& c)
 
 bool Lexico::isLyrics(const char& c)
 {
-	return isalpha(c) || c == 'ñ';
+	return isalpha(c) || c == 'ñ' || c== 'Ñ';
 }
 
 bool Lexico::isPoint(const char& c)
@@ -163,9 +193,12 @@ string Lexico::chainResult()
 void Lexico::behind()
 {
 	cout << "Se  obtuvo: " << result << " ";
+	if (state == 3) {
+		state = 1;
+	}
 	cout << chainResult() << endl;
-
 	result = "";
 	cout << c << ": Interrumpio la secuecia, vuelve al estado inicial" << endl;
 	state = 0;
+	
 }
